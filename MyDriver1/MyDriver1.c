a4fdc6ace18c80331d3936aa81966118210dcde4
zhoudongdong <zhoudongdong@rongma.com>
2023-04-28 12:03:30 +0800

    write 出错，驱动代码96行

--- MyDriver1/MyDriver1.c
+++ MyDriver1/MyDriver1.c
@@ -4,6 +4,8 @@
 #define DEVICE_NAME L"\\Device\\MyFirstDevice"
 #define SYM_NAME L"\\??\\MyFirstDevice"
 
+#define IOCTL_MUL CTL_CODE(FILE_DEVICE_UNKNOWN, 0X9888, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
 void DrvUnload(PDRIVER_OBJECT pdriver)
 {
 	DbgPrint("Unload..\n");
@@ -19,41 +21,128 @@ void DrvUnload(PDRIVER_OBJECT pdriver)
 
 }
 
-NTSTATUS MyCreate(IN PDEVICE_OBJECT dev, IN PIRP irp)
+NTSTATUS MyCreate(IN PDEVICE_OBJECT dev, IN PIRP pirp)
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
 	DbgPrintEx(77,0,"My device has be created!\n");
 
-	irp->IoStatus.Information = 0;
-	irp->IoStatus.Status = status;
-	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	pirp->IoStatus.Information = 0;
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
 
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS MyClose(IN PDEVICE_OBJECT dev, IN PIRP irp)
+NTSTATUS MyClose(IN PDEVICE_OBJECT dev, IN PIRP pirp)
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
 	DbgPrintEx(77, 0, "My device has be closed!\n");
 
-	irp->IoStatus.Information = 0;
-	irp->IoStatus.Status = status;
-	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	pirp->IoStatus.Information = 0;
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
 
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS MyCleanup(IN PDEVICE_OBJECT dev, IN PIRP irp)
+NTSTATUS MyCleanup(IN PDEVICE_OBJECT dev, IN PIRP pirp)
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
 	DbgPrintEx(77, 0, "My device has be cleanup!\n");
 
-	irp->IoStatus.Information = 0;
-	irp->IoStatus.Status = status;
-	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	pirp->IoStatus.Information = 0;
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS MyRead(IN PDEVICE_OBJECT dev, IN PIRP pirp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrintEx(77, 0, "My device has be Readed!\n");
+
+	PIO_STACK_LOCATION pstack = IoGetCurrentIrpStackLocation(pirp);
+	ULONG readlen = pstack->Parameters.Read.Length;
+	PCHAR read_buff = pirp->AssociatedIrp.SystemBuffer;
+
+	RtlCopyMemory(read_buff, "This is a kernel message.", strlen("This is a kernel message."));
+
+	pirp->IoStatus.Information = strlen("This is a kernel message.");
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
+
+	DbgPrintEx(77, 0, "Readed succeed!\n");
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS MyWrite(IN PDEVICE_OBJECT dev, IN PIRP pirp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrintEx(77, 0, "My device has be Writed!\n");
+
+	__debugbreak();
+
+	PIO_STACK_LOCATION pstack = IoGetCurrentIrpStackLocation(pirp);
+	ULONG writelen = pstack->Parameters.Write.Length;
+	PCHAR write_buff = pirp->AssociatedIrp.SystemBuffer;
+
+	RtlZeroMemory(dev->DeviceExtension, 200);
+	RtlCopyMemory(dev->DeviceExtension, write_buff, writelen);
+	DbgPrintEx(77, 0, "%s\n", (PCHAR)dev->DeviceExtension);
+
+	pirp->IoStatus.Information = 13;
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
+
+	DbgPrintEx(77, 0, "Write succeed!\n");
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS MyControl (IN PDEVICE_OBJECT dev, IN PIRP pirp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrintEx(77, 0, "My device has be control!\n");
+
+	PIO_STACK_LOCATION pstack = IoGetCurrentIrpStackLocation(pirp);
+
+	ULONG iocode = pstack->Parameters.DeviceIoControl.IoControlCode;	
+	ULONG inlen = pstack->Parameters.DeviceIoControl.InputBufferLength;
+	ULONG outlen = pstack->Parameters.DeviceIoControl.OutputBufferLength;
+
+	ULONG ioinfo = 0;
+
+	switch(iocode){
+
+	case IOCTL_MUL:
+	{
+		DWORD64 indata = *(PDWORD64)pirp->AssociatedIrp.SystemBuffer;
+		indata = indata * 5;
+		*(PDWORD64)pirp->AssociatedIrp.SystemBuffer = indata;
+
+		ioinfo = 7777;
+		break;
+	}
+		
+	default:
+		status = STATUS_UNSUCCESSFUL;
+		ioinfo = 0;
+		break;
+	}
+
+	pirp->IoStatus.Information = ioinfo;
+	pirp->IoStatus.Status = status;
+	IoCompleteRequest(pirp, IO_NO_INCREMENT);
+
+	DbgPrintEx(77, 0, "IoCtl succeed!\n");
 
 	return STATUS_SUCCESS;
 }
@@ -68,7 +157,6 @@ NTSTATUS DriverEntry(
 	UNICODE_STRING device_name = { 0 };
 	PDEVICE_OBJECT pdevice = NULL;
 
-	__debugbreak();
 
 	driver->DriverUnload = DrvUnload;
 
@@ -81,6 +169,8 @@ NTSTATUS DriverEntry(
 
 		return status;
 	}
+
+	pdevice->Flags |= DO_BUFFERED_IO;
 
 	// 创建设备成功，创建符号连接
 	UNICODE_STRING sym_name = { 0 };
@@ -101,6 +191,12 @@ NTSTATUS DriverEntry(
 	driver->MajorFunction[IRP_MJ_CLOSE] = MyClose;
 
 	driver->MajorFunction[IRP_MJ_CLEANUP] = MyCleanup;
+
+	driver->MajorFunction[IRP_MJ_READ] = MyRead;
+
+	driver->MajorFunction[IRP_MJ_WRITE] = MyWrite;
+
+	driver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = MyControl;
 
 	return STATUS_SUCCESS;
 }
